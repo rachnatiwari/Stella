6a6486c83fdda41356d8341270a48da427d4616d
Rachna Tiwari <apple@Rachna-Tiwari.local>
2020-08-09 12:19:00 +0530

    Added MongoDB for Website Database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter,Route,Link} from 'react-router-dom';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 function App() {
 
@@ -45,6 +46,7 @@ function App() {
                 <main className="main">
                     <div className="content">
                     <Route path="/products/:id" component={ProductScreen} />
+                    <Route path="/cart/:id?" component={CartScreen} />
                     <Route path="/" exact={true} component={HomeScreen} />
                     </div>
                 </main>
